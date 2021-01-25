51345dddea5f159db6981c07178ca8749ccca132
Sharday <sbriscoe3420@aol.com>
2021-01-24 22:03:34 -0600

    Jan24

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
+from django.http import Http404
+
 
 def home(request):
 	boards = Board.objects.all()
 	return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+	# we included pk in the url.py file so we need it here
+	# pk means primary key
+	board = get_object_or_404(Board, pk=pk)
+	return render(request, 'topics.html', {'board': board})

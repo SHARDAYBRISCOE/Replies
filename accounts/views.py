cf73d6aeb9df80ce136af8a723706fab70d0c6dc
Sharday <sbriscoe3420@aol.com>
2021-01-31 00:19:18 -0600

    SignUpPageCreated

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.contrib.auth import login as auth_login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import SignUpForm
 # Create your views here.
 def signup(request):
-	return render(request, 'signup.html')
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid(): 
+			user = form.save()
+			#user instance created
+			auth_login(request, user)
+			return redirect('home')
+	else:
+		form = SignUpForm()
+	return render(request, 'signup.html', {'form': form})
